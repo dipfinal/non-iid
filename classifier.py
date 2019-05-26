5bb7026df5473e40ce1dcba43f0fa340be2fd999
xtx <xtx17@mails.tsinghua.edu.cn>
2019-05-27 00:40:02 +0800

    create basic classfier

---
+++ classifier.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Classfier:
+
+    def __init__():
+        pass
+
+    def train(X,Y):
+        """
+        X : ndarray, shape = (n_sample,n_dim=50) train_data
+        Y : ndarray, shape = (n_sample,n_dim=2 ) train_label
+        """
+        pass
+
+    def predict(X):
+        """
+        X : ndarray, shape = (n_sample,n_dim=50) val_data
+        return Y: ndarray,shape = (n_sample,) predict_label for object
+        """
+        pass
+
