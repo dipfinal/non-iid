ba04d0fa53e4f49780db7f28f31d783170036511
xtx <xtx17@mails.tsinghua.edu.cn>
2019-05-27 19:11:45 +0800

    add classifier

--- classifier.py
+++ classifier.py
@@ -2,17 +2,17 @@ import numpy as np
 
 class Classifier:
 
-    def __init__():
+    def __init__(self):
         pass
 
-    def train(X,Y):
+    def train(self,X,Y):
         """
         X : ndarray, shape = (n_sample,n_dim=50) train_data
         Y : ndarray, shape = (n_sample,n_dim=2 ) train_label
         """
         pass
 
-    def predict(X):
+    def predict(self,X):
         """
         X : ndarray, shape = (n_sample,n_dim=50) val_data
         return Y: ndarray,shape = (n_sample,) predict_label for object
