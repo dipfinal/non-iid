b73c5aeea4ee2fb3001e82d404e57eb3e970a75e
xtx <xtx17@mails.tsinghua.edu.cn>
2019-05-29 21:22:26 +0800

    change

--- Classifiers.py
+++ Classifiers.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
+from CRLR import mainFunc
 class KNNClassifier(Classifier):
     def __init__(self):
         pass
@@ -77,5 +78,49 @@ class SVMClassifier(Classifier):
             Y = np.concatenate((Y,clf.predict_proba(X)[:,1][:,np.newaxis]),axis=1)
 
         Y = np.argmax(Y,axis=1)
+        return Y
+
+class CRLR_SVM(Classifier):
+    lambda0 = 1  # Logistic loss
+    lambda1 = 0.1  # Balancing loss
+    lambda2 = 1  # L_2 norm of sample weight
+    lambda3 = 0  # L_2 norm of beta
+    lambda4 = 0.001  # L_1 norm of bata
+    lambda5 = 1  # Normalization of sample weight
+    MAXITER = 1000
+    ABSTOL = 1e-3
+
+    def __init(self):
+        pass
+    def train(self,X,Y):
+        self.clfs = [SVC(gamma="auto", kernel='rbf', class_weight='balanced', probability=True) for i in range(10)]
+        for i in range(10):
+            Y_ = Y[:, 0].copy()
+            for j in range(Y_.shape[0]):
+                if (Y_[j] == i):
+                    Y_[j] == 1
+                else:
+                    Y_[j] == 0
+            #print(Y_.shape)
+            Y_ = Y_[:,np.newaxis]
+            n_sample = X.shape[0]
+            n_feature = X.shape[1]
+            W_init = np.random.rand(n_sample, 1)
+            beta_init = 0.5 * np.ones([n_feature, 1])
+            W, beta, J_loss = mainFunc(X, Y_, \
+                                       self.lambda0, self.lambda1, self.lambda2, self.lambda3, self.lambda4, self.lambda5, \
+                                       1000, self.ABSTOL, W_init, beta_init)
+
+            for i in range(n_sample):
+                for j in range(n_feature):
+                    X[i][j] *= beta[j]
+            self.clfs[i].fit(X, Y_,sample_weight=np.squeeze(beta))
+
+    def predict(self, X):
+        Y = np.empty(shape=(X.shape[0], 0))
+        for clf in self.clfs:
+            Y = np.concatenate((Y, clf.predict_proba(X)[:, 1][:, np.newaxis]), axis=1)
+
+        Y = np.argmax(Y, axis=1)
         return Y
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-from KNNClassifier import *
+from Classifiers import *
+from CRLR import mainFunc
 def accuracy(Y,Y_):
     return 1.0*np.sum(Y==Y_)/Y.shape[0]
 
@@ -8,7 +9,12 @@ if __name__ =="__main__":
     trainY = np.load("./trainY.npy")
     valX = np.load("./valX.npy")
     valY = np.load("./valY.npy")
-    clf = SVMClassifier()
+    clf = CRLR_SVM()
     clf.train(trainX,trainY)
     Y = clf.predict(valX)
+
+
+    #clf = SVMClassifier()
+    #clf.train(trainX,trainY)
+    #Y = clf.predict(valX)
     print(accuracy(Y,valY[:,0]))
