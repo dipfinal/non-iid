11f84fe3fdd7ec3dea4d97d8f09e29996875d8c1
xtx <xtx17@mails.tsinghua.edu.cn>
2019-05-30 17:37:16 +0800

    debug CRLR

--- CRLR.py
+++ CRLR.py
@@ -30,7 +30,7 @@ def balance_cost(W=None,X=None,*args,**kwargs):
     for i in np.arange(0,m):
         X_sub=np.copy(X)
         X_sub[:,i]=0
-        I=(X[:,i] > 0).astype('double')+10e-4
+        I=(X[:,i] > 0).astype('double')
         loss=( np.dot( X_sub.T, np.multiply( np.multiply(W,W),I.reshape(-1,1) ) ) ) / (np.dot((np.multiply(W,W)).T,I.reshape(-1,1)))\
             -(np.dot(X_sub.T,(np.multiply((np.multiply(W,W)),(1 - I.reshape(-1,1)))))) / (np.dot((np.multiply(W,W)).T,(1 - I.reshape(-1,1))))
         #print (loss.shape)
@@ -42,16 +42,16 @@ def balance_grad(W=None,X=None,*args,**kwargs):
 
     g_w=np.zeros([n,m])
     for i in range(0,m):
-        X_sub = X;
-        X_sub[:,i] = 0; # the ith column is treatment
-        I = (X[:,i]>0).reshape(-1,1).astype('double')+10e-4;
+        X_sub = np.copy(X)
+        X_sub[:,i] = 0 # the ith column is treatment
+        I = (X[:,i]>0).reshape(-1,1).astype('double')
         J1 = (X_sub.T@((W*W)*I.reshape(-1,1)))/((W*W).T@(I.reshape(-1,1))) \
-            -(X_sub.T@((W*W)*(1-I).reshape(-1,1)))/((W*W).T@(1-I).reshape(-1,1));
+            -(X_sub.T@((W*W)*(1-I).reshape(-1,1)))/((W*W).T@(1-I).reshape(-1,1))
         dJ1W = 2*(X_sub.T*((W*I)@np.ones([1,m])).T*((W*W).T@I) \
                   -(X_sub.T@(((W*W)*I)@(W*I).T)))/((W*W).T@I)**2 \
                   -2*(X_sub.T*((W*(1-I))@np.ones([1,m])).T*((W*W).T@(1-I)) \
-                  -((X_sub.T@( (W*W) * (1-I) )) @  (W*(1-I) ).T ))/((W*W).T@(1-I))**2;
-        g_w[:,i] = (2 * dJ1W.T @ J1).ravel();
+                  -((X_sub.T@( (W*W) * (1-I) )) @  (W*(1-I) ).T ))/((W*W).T@(1-I))**2
+        g_w[:,i] = (2 * dJ1W.T @ J1).ravel()
 
     return g_w
 
@@ -64,75 +64,75 @@ def mainFunc(X, Y, \
     MAXITER, ABSTOL, W_init, beta_init):
 
     n,m = X.shape
-    W = W_init;
-    W_prev = W;
-    beta = beta_init;
-    beta_prev = beta;
+    W = W_init
+    W_prev = np.copy(W)
+    beta = beta_init
+    beta_prev = np.copy(beta)
 
-    parameter_iter = 0.5;
-    J_loss = np.ones([MAXITER, 1])*(-1);
+    parameter_iter = 0.5
+    J_loss = np.ones([MAXITER, 1])*(-1)
 
-    lambda_W = 1;
-    lambda_beta = 1;
+    lambda_W = 1
+    lambda_beta = 1
 
-    W_All = np.zeros([n, MAXITER]);
-    beta_All = np.zeros([m, MAXITER]);
+    W_All = np.zeros([n, MAXITER])
+    beta_All = np.zeros([m, MAXITER])
 
 
     # Optimization with gradient descent
     for iter in range(1,MAXITER+1):
         print("iter=",iter)
         # Update beta
-        y = beta;
-        beta = beta + (iter/(iter+3))*(beta-beta_prev); # fast proximal gradient
-        f_base = J_cost(W, beta, X, Y, lambda0, lambda1, lambda2, lambda3, lambda5);
+        y = beta.copy()
+        beta = beta + (iter/(iter+3))*(beta-beta_prev)
+        f_base = J_cost(W, beta, X, Y, lambda0, lambda1, lambda2, lambda3, lambda5)
         grad_beta = lambda0*(((sigmoid(X@beta)-Y)*(W*W)).T@X).T \
-                   +2*lambda3*beta;
+                   +2*lambda3*beta
 
         while 1:
-            z = prox_l1(beta - lambda_beta*grad_beta, lambda_beta*lambda4);
+            z = prox_l1(beta - lambda_beta*grad_beta, lambda_beta*lambda4)
             if J_cost(W, z, X, Y, lambda0, lambda1, lambda2, lambda3, lambda5)\
                <= f_base + grad_beta.T@(z-beta)\
                + (1/(2*lambda_beta))*sum((z-beta)**2):
-                break;
-            lambda_beta = parameter_iter*lambda_beta;
+                break
+            lambda_beta = parameter_iter*lambda_beta
             if lambda_beta<1e-100:
                 break
-        beta_prev = y;
-        beta = z;
+        beta_prev = y
+        beta = z
 
         # Update W
-        y = W;
-        W = W+(iter/(iter+3))*(W-W_prev);
-        f_base = J_cost(W, beta, X, Y, lambda0, lambda1, lambda2, lambda3, lambda5);
+        y = np.copy(W)
+        W = W+(iter/(iter+3))*(W-W_prev)
+        f_base = J_cost(W, beta, X, Y, lambda0, lambda1, lambda2, lambda3, lambda5)
 
         grad_W = 2*lambda0*(np.log(1+np.exp(X@beta))-Y*(X@beta))*W \
                 +lambda1*balance_grad(W, X)@np.ones([m,1]) \
                 +4*lambda2*W*W*W \
-                +4*lambda5*(sum(W*W)-1)*W;
+                +4*lambda5*(sum(W*W)-1)*W
 
         while 1:
-            z = prox_l1(W-lambda_W*grad_W, 0);
+            z = prox_l1(W-lambda_W*grad_W, 0)
             if J_cost(z, beta, X, Y, lambda0, lambda1, lambda2, lambda3, lambda5)\
                     <= f_base + grad_W.T@(z-W)\
                     + (1/(2*lambda_W))*sum((z-W)**2):
-                break;
-            lambda_W = parameter_iter*lambda_W;
+                break
+            lambda_W = parameter_iter*lambda_W
             if lambda_W<1e-100:
                 break
-        W_prev = y;
-        W = z;
+        W_prev = y
+        W = z
 
-        W_All[:,iter-1] = W.ravel();
-        beta_All[:,iter-1] = beta.ravel();
+        W_All[:,iter-1] = W.ravel()
+        beta_All[:,iter-1] = beta.ravel()
 
         J_loss[iter-1] = J_cost(W, beta, X, Y,\
                               lambda0, lambda1, lambda2, lambda3, lambda5)\
-                     + lambda4*sum(abs(beta));
+                     + lambda4*sum(abs(beta))
 
         if (iter > 1) & ( abs(J_loss[iter-1] - J_loss[iter-2])[0]  < ABSTOL) or (iter == MAXITER):
             break
-    W = W*W;
+    W = W*W
 
     return W, beta, J_loss
 
@@ -155,17 +155,17 @@ def report_metrics(y_test, y_pred):
 
 
 if __name__ == '__main__':
-    X = 2*np.round(np.random.rand(1000, 20))-1; # 1000 samples and 20 features
-    beta_true = np.ones([20, 1]);
-    Y = (sigmoid(np.dot(X,beta_true))>=0.5).astype('double');
-    lambda0 = 1; #Logistic loss
-    lambda1 = 0.1; #Balancing loss
-    lambda2 = 1; #L_2 norm of sample weight
-    lambda3 = 0; #L_2 norm of beta
-    lambda4 = 0.001; #L_1 norm of bata
-    lambda5 = 1; #Normalization of sample weight
-    MAXITER = 1000;
-    ABSTOL = 1e-3;
+    X = 2*np.round(np.random.rand(1000, 20))-1 # 1000 samples and 20 features
+    beta_true = np.ones([20, 1])
+    Y = (sigmoid(np.dot(X,beta_true))>=0.5).astype('double')
+    lambda0 = 1 #Logistic loss
+    lambda1 = 0.1 #Balancing loss
+    lambda2 = 1 #L_2 norm of sample weight
+    lambda3 = 0 #L_2 norm of beta
+    lambda4 = 0.001 #L_1 norm of bata
+    lambda5 = 1 #Normalization of sample weight
+    MAXITER = 1000
+    ABSTOL = 1e-3
 
     print ('***********classic logistic***********')
     X_train, X_test, y_train, y_test = train_test_split(X,Y.astype('int'))
@@ -176,8 +176,8 @@ if __name__ == '__main__':
     report_metrics(y_test, y_pred)
 
     print ('***********CRLR***********')
-    W_init = np.random.rand(X_train.shape[0], 1);
-    beta_init = 0.5*np.ones([20, 1]);
+    W_init = np.random.rand(X_train.shape[0], 1)
+    beta_init = 0.5*np.ones([20, 1])
 
     W, beta, J_loss = mainFunc(X_train, y_train,\
             lambda0, lambda1, lambda2, lambda3, lambda4, lambda5,\
